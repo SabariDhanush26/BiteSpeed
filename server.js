b7eca5764818711b739e3c4042e045909c4df5f4
Sabari Dhanush <sabaripssd@gmail.com>
2024-01-25 00:01:00 +0530

    final changes

--- server.js
+++ server.js
@@ -57,36 +57,42 @@ app.post("/identify", (req, res) => {
        .then(result => {
         // res.send("done");
         // console.log('Updated query result:', result);
+        function allValues() {
+          return User.findAndCountAll({where:{[Op.or]: [
+          {email:data.email},
+          {phoneNumber:data.phoneNumber},
+        ]}
+      });}
+      allValues()
+      .then(({count,rows})=>{
+        console.log(rows);
+        primaryID=rows[0].dataValues.id;
+        emails.push(rows[0].dataValues.email);
+        phoneNumbers.push(rows[0].dataValues.phoneNumber);
+        for(let i=1;i<count;i++){
+          emails.push(rows[i].dataValues.email);
+          phoneNumbers.push(rows[i].dataValues.phoneNumber);
+          secondaryContactIDs.push(rows[i].dataValues.id);
+        }
+        emails=[...new Set(emails)];
+        phoneNumbers=[...new Set(phoneNumbers)];
+        secondaryContactIDs=[...new Set(secondaryContactIDs)];
+        // console.log(count,rows[0].dataValues);
+        res.send({
+          contact:{
+            "primaryContactId":primaryID,
+            "emails":emails,
+            "phoneNumbers":phoneNumbers,
+            "secondaryContactIds":secondaryContactIDs
+          }
+        })
+      })
        }
         )
        .catch(error => {
         console.error('Error executing UPDATE query:', error);
       });})
       console.log('User found:', foundUser);
-      function allValues() {
-        return User.findAndCountAll({where:{[Op.or]: [
-        {email:data.email},
-        {phoneNumber:data.phoneNumber},
-      ]}
-    });}
-    allValues()
-    .then(({count,rows})=>{
-      primaryID=rows[0].dataValues.id;
-      for(let i=1;i<count;i++){
-        emails.push(rows[i].dataValues.email);
-        phoneNumbers.push(rows[i].dataValues.phoneNumber);
-        secondaryContactIDs.push(rows[i].dataValues.id);
-      }
-      // console.log(count,rows[0].dataValues);
-      res.send({
-        contact:{
-          "primaryContactId":primaryID,
-          "emails":emails,
-          "phoneNumbers":phoneNumbers,
-          "secondaryContactIds":secondaryContactIDs
-        }
-      })
-    })
     } else {
       function creation(){
         return User.create(data);
