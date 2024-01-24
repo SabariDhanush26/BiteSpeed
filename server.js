63589fdfb8099ff84a43f887cf1072c3e423e8c2
Sabari Dhanush <sabaripssd@gmail.com>
2024-01-24 10:56:19 +0530

    post API done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { Sequelize } from "sequelize";
+import { Sequelize, Op  } from "sequelize";
 import defineUserModel from "./Models/User.js";
 import { constants } from "./config.js";
 const sequelize = new Sequelize(constants.development);
@@ -23,27 +23,59 @@ const app = express();
 const port = constants.development.PORT || 3000;
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
-  res.send(`Server Started at Port: ${port}`);
+  (async () => {
+    await sequelize.sync();
+    return User.findAll({ raw: true });
+  })().then((allUsers) => {
+    res.send(allUsers);
+  });
+  // res.status(200).json({ message: `Server Started at Port: ${port}` });
 });
 app.listen(port, () => console.log(`Server Started at ${port}`));
+// app.post("/identify", (req, res) => {
+//   const data = req.body;
+//   (async () => {
+//     await sequelize.sync();
+//     const incomingUser = await User.create(data);
+//     const existingUser = User.findOne({where:{phoneNumber:data.phoneNumber}})||User.findOne({where:{email:data.email}})
+//     console.log(existingUser)
+//     .then((existingUser) => {
+//         if (existingUser) {
+//             incomingUser.set({
+//                 linkPrecedence:"secondary",
+//                 linkedId:existingUser.id
+//             })
+//             incomingUser.save();
+//         } else {
+//             console.log("newUser Created Succesfully");
+//         }
+//       })
+//   })();
+//   res.status(200).json({ message: "Data received successfully" });
+// });
 
 app.post("/identify", (req, res) => {
   const data = req.body;
-  (async () => {
-    await sequelize.sync();
-    const incomingUser = await User.create(data);
-    const existingUser = User.findOne({where:{phoneNumber:data.phoneNumber}})||User.findOne({where:{email:data.email}})
-    .then((existingUser) => {
-        if (existingUser) {
-            incomingUser.set({
-                linkPrecedence:"secondary",
-                linkedId:existingUser.id
-            })
-            incomingUser.save();
-        } else {
-            console.log("newUser Created Succesfully");
-        }
-      })
-  })(); 
-  res.status(200).json({ message: "Data received successfully" });
+  sequelize.sync()
+  .then(() => {
+    console.log('User model synced with the PostgreSQL database.');
+    // Find one user
+    return User.findOne( {where:{[Op.or]: [
+      {email:data.email},
+      {phoneNumber:data.phoneNumber},
+    ]}
+  });
+})
+  .then((foundUser) => {
+    console.log(foundUser);
+    if (foundUser) {
+      console.log('User found:', foundUser.toJSON());
+    } else {
+      console.log('No user found.');
+    }
+  })
+  .catch((err) => {
+    console.error('Error syncing User model:', err);
+  });
+  res.send("done");
 });
